1f16c7732469f4051d69937993a87bc6c0076011
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-14 00:47:44 +0500

    Rename main class

--- src/main/java/com/example/demo/SampleApplication.java
+++ src/main/java/com/example/demo/SampleApplication.java
@@ -2,14 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 
-public class DemoApplication {
+public class SampleApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SampleApplication.class, args);
     }
 }
