13c6e720d8dc27e5fdf6ebb794286b7fa532b0f0
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-14 00:45:23 +0500

    Extract config and rest controller from demo application

---
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Simple REST controller
+ */
+@RestController
+public class SampleController {
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+}
