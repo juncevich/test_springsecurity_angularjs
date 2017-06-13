9a2cea3e8b6a426a674bca8bca0e2fb17b3a4448
Alexandr Juncevich <a.juncevich@gmail.com>
2017-06-13 19:01:22 +0500

    Add CSRF

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,11 +31,12 @@ public class DemoApplication {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
-                    .httpBasic()
-                    .and()
+                    .httpBasic().and()
                     .authorizeRequests()
-                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-                    .anyRequest().authenticated();
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
+                    .authenticated().and()
+                    .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
         }
     }
 
